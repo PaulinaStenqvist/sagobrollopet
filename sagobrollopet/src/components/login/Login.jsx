a4e97a35c0f764075170897fd97b4189e6d3ac44
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-02-06 20:02:16 +0100

    update login

--- sagobrollopet/src/components/login/Login.jsx
+++ sagobrollopet/src/components/login/Login.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import styles from "./login.module.css";
-
+import { FaUser, FaLock } from "react-icons/fa";
 const Login = () => {
     return(
-     <div className={styles.container}>
-        Login
+     <div className="wrapper">
+        <form action="">
+         <h1>Logga In</h1>
+         <div className="input-box">
+            <input type="text" placeholder='Användarnamn' required />
+            <FaUser className='icon' />
+         </div>
+         <div className="input-box">
+            <input type="text" placeholder='Lösenord' required />
+            <FaLock className='icon' />
+         </div>
+         <div className="remember-forgot">
+            <label> <input type="checkbox" />Kom ihåg mig</label>
+            <a href="a">Glömt Lösenord</a>
+         </div>
+
+         <button type="submit">Logga In</button>
+         <div className="register-link">
+            <p>Har inget konto <a href="a">Registera</a></p>
+         </div>
+        </form>
      </div>
     );
     };
