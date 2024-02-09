5b1df0496ea759e6b9fcdf296abe737948e513ee
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-02-09 13:21:55 +0100

    update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,57 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './header.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
-const Header = () => {
+export const Header = () => {
 
- const [isMobile, setIsMobile] = useState(false);
+ const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="header">
       <h3 className="logo">Sagobröllopet</h3>
-      <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
-        onClick={() => setIsMobile(false)}
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+        <ul className={menuOpen ? "open" : ""}>
         <Link to="/" className="home">
           <li>HOME</li>
         </Link>
+        <li>
+          <NavLink to="/about">Om</NavLink>
+        </li>
+        <li>
+          <NavLink to="/services">Portfolio</NavLink>
+        </li>
+        <li>
+          <NavLink to="/services">Priser</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Kontakt</NavLink>
+        </li>
+        <li>
+          <NavLink to="/services">Logga In</NavLink>
+        </li>
       </ul>
-      <ul className="nav-links">
-        <Link to="/about" className="about">
-          <li>OM</li>
-        </Link>
-      </ul>
-      <ul className="nav-links">
-        <Link to="/portfolio" className="portfolio">
-          <li>PORTFOLIO</li>
-        </Link>
-      </ul>
-      <ul className="nav-links">
-        <Link to="/pricing" className="pricing">
-          <li>PRIS</li>
-        </Link>
-      </ul>
-      <ul className="nav-links">
-        <Link to="/contact" className="contact">
-          <li>KONTAKT</li>
-        </Link>
-      </ul>
-      <ul className="nav-links">
-        <Link to="/login" className="login">
-          <li>LOGGA IN</li>
-        </Link>
-      </ul>
-      <button 
-      className="mobile-menu-icon"
-      onClick={() => setIsMobile(!isMobile)}
-      >
-        {isMobile ? (
-        <i className="fas fa-times"></i>
-      ) : (
-        <i className="fas fa-bars"></i>
-      )}
-      </button>
     </nav>
   );
 };
 
-export default Header;
 
