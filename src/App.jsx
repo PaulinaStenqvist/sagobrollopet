89df6066d668b3992bd7b2e0e8d9f20cb76625fa
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-02-12 18:58:19 +0100

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/Header';
-// import Home from './components/home/Home';
-import About from './components/about/About';
-import Portfolio from './components/portfolio/Portfolio';
-import Pricing from './components/pricing/Pricing';
-import Contact from './components/contact/Contact';
-import Login from './components/login/Login';
-//import Services from './components/services/Services'; 
-import Footer from './components/footer/Footer';
-
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import { Navbar } from "./components/Navbar";
-import { About, Contact, Home, Services } from "./components/pages";
+import { About } from "./components/about/About";
+import { Portfolio } from "./components/portfolio/Portfolio";
+import { Contact } from "./components/contact/Contact";
+import { Login } from "./components/login/Login";
 
 function App() {
   return (
