464c05dbf74fe6d0fcee514c72c0e03a55a2b145
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-02-13 08:52:06 +0100

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
-import { Header } from "./components/header/Header";
-import { About } from "./components/about/About";
-import { Portfolio } from "./components/portfolio/Portfolio";
-import { Contact } from "./components/contact/Contact";
-import { Login } from "./components/login/Login";
+import  Header  from "./components/header/Header";
+import  About  from "./components/about/About";
+import  Portfolio  from "./components/portfolio/Portfolio";
+import  Contact  from "./components/contact/Contact";
+import  Login  from "./components/login/Login";
 
 function App() {
-  return (
+  return (  
     <div className="App">
       <Header />
       <Routes>
